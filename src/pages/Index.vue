c5ccf8b027b9238a13ae1f63e8bd9d1b0c01dcb7
Ilya.Gordeev <gordeevilyas@yandex.ru>
2023-06-18 16:02:25 +0300

    Separate styles from components

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,39 +23,3 @@ export default {
     },
 }
 </script>
-
-<style scoped lang="sass">
-.main
-    height: 92vh
-    .header
-        background: #505050
-        justify-content: space-between
-        display: flex
-        flex-direction: row
-        padding: 6px
-        height: fit-content
-        &__title
-            color: #efefef
-            padding: 8px 16px
-            letter-spacing: .2rem
-            font-size: 32px
-    .content
-        display: flex
-        align-items: center
-        align-content: center
-        height: 100%
-        justify-content: center
-        &__button
-            cursor: pointer
-            font-size: 32px
-            color: #ffffff
-            text-shadow: 1px 1px 1px #272625
-            padding: 10px 18px
-            box-shadow: 0 0 10px #575757
-            border-radius: 5px
-            border: none
-            background: #f66d52
-            &:hover
-                color: #ffffff
-                background: #398945
-</style>
